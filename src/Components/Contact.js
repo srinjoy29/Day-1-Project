c52fd9bd5888712a350c4782bb6cb573cfbb5577
ManishKumar1314 <123481425+ManishKumar1314@users.noreply.github.com>
2024-09-04 12:25:38 +0530

    updated contact

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -6,7 +6,10 @@ const Contact = () => {
       <h1 className="primary-heading">Have Question In Mind?</h1>
       <h1 className="primary-heading">Let Us Help You</h1>
       <div className="contact-form-container">
+<<<<<<< HEAD
+        
         <input type="text" placeholder="gulshan7519760037@gmail.com" />
+>>>>>>> e9d18d31c5a13c01c5001ea3ac93f503b9143e10
         <button className="secondary-button">Submit</button>
       </div>
     </div>

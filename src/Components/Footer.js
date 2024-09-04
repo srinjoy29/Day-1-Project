898e05869b11fdbcbeaf01506b330bb14bfa7bc6
Srinjoy Das <96923080+srinjoy29@users.noreply.github.com>
2024-09-04 12:24:51 +0530

    updated

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,4 @@
 import React from "react";
-import Logo from "../Assets/Logo.svg";
-import { BsTwitter } from "react-icons/bs";
-import { SiLinkedin } from "react-icons/si";
 import { BsYoutube } from "react-icons/bs";
 import { FaFacebookF } from "react-icons/fa";
 

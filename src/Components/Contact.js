837bfb66001aee4230de560acf5e9741ac0df60c
Srinjoy Das <96923080+srinjoy29@users.noreply.github.com>
2024-09-04 12:29:32 +0530

    update contact

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -4,12 +4,9 @@ const Contact = () => {
   return (
     <div className="contact-page-wrapper">
       <h1 className="primary-heading">Have Question In Mind?</h1>
-      <h1 className="primary-heading">Let Us Help You</h1>
+      <h1 className="primary-heading">Let Us Know</h1>
       <div className="contact-form-container">
-<<<<<<< HEAD
-        
-        <input type="text" placeholder="gulshan7519760037@gmail.com" />
->>>>>>> e9d18d31c5a13c01c5001ea3ac93f503b9143e10
+        <input type="text" placeholder="Your Mail" />
         <button className="secondary-button">Submit</button>
       </div>
     </div>
